fba6aa71b5d87be63d5c2da951bc259012d65292
Matt Grigsby <heyahem@hotmail.com>
2020-08-13 11:59:46 +0100

    Updates test properties to reflect creation of data

---
+++ JavaPerfMon/JavaPerfMonApp/src/main/resources/data-h2.sql
@@ -0,0 +1,6 @@
+MERGE INTO endpoint (`id`,`site`,`enabled`,`todaylowcount`) VALUES 
+(1,'www.google.co.uk',1,0),
+(2,'www.bbc.co.uk',1,0),
+(3,'swapi.co',1,0),
+(4,'swapi.dev',1,0),
+(5,'ghibliapi.herokuapp.com',1,0);
